e0cb723aad83a9857c7294cb5a8564e63948e15d
lgposso23 <163008630+lgposso23@users.noreply.github.com>
2024-03-14 15:32:24 -0500

    Add files via upload

---
+++ scripts/before_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Instalar dependencias de Node.js (si es necesario)
+# Este script se ejecuta antes de que el nuevo código se despliegue.
+cd /home/ubuntu
+npm install
---
+++ scripts/start_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Iniciar el servidor Node.js
+cd /home/ubuntu
+nohup sudo node server.js > /dev/null 2>&1 &
