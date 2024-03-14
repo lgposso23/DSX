f851010a129dd1389f8acfb2c03819b2bf4f572f
lgposso23 <163008630+lgposso23@users.noreply.github.com>
2024-03-14 15:59:23 -0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Instalar dependencias de Node.js (si es necesario)
 # Este script se ejecuta antes de que el nuevo código se despliegue.
-cd /home/ubuntu
 npm install dgram
 npm install mysql
 npm install socket.io
