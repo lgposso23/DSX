c7e9d1bc25fbac1467176494bdf4171363fa7e67
lgposso23 <163008630+lgposso23@users.noreply.github.com>
2024-03-14 20:12:49 -0500

    Update and rename stop_server.sh to after_install.sh

---
+++ scripts/after_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ubuntu/myapp
+
+# Usar npm para instalar las dependencias específicas
+npm install express http socket.io dgram mysql
--- scripts/stop_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Detener el servidor Node.js actual
-# Este es un enfoque muy básico. Considera utilizar PM2 o un enfoque similar para gestionar el proceso.
-pkill -f "node server.js"
