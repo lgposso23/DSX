ebb34ed622773acf87c3cd50427ec7c660e4c2ea
Mighty-Fight <amantillac@uninorte.edu.co>
2024-04-13 15:49:51 -0500

    modified:   public/Equipo_trabajo.js

--- public/Equipo_trabajo.js
+++ public/Equipo_trabajo.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Selecciona automáticamente la opción "Equipo de Trabajo" en el menú desplegable
-    document.getElementById('opcionesMenu').value = 'equipoTrabajo';
-});
+    // Obtener el elemento del menú desplegable
+    var menuDesplegable = document.getElementById('opcionesMenu');
 
-// Función para manejar los cambios en el menú desplegable
-function cambiarOpcion() {
-    var opcionSeleccionada = document.getElementById("opcionesMenu").value;
-    // Aquí puedes agregar la lógica para manejar la opción seleccionada
-    console.log("Opción seleccionada:", opcionSeleccionada);
-}
+    // Agregar un listener al cambio de opción en el menú desplegable
+    menuDesplegable.addEventListener('change', function(event) {
+        // Obtener el valor seleccionado del menú desplegable
+        const seleccionado = event.target.value;
+
+        // Verificar el valor seleccionado y redirigir a la página correspondiente
+        if (seleccionado === 'rastreoHistoricos') {
+            window.location.href = '/historicos.html';
+        } else if (seleccionado === 'Principal') {
+            window.location.href = '/principal.html';
+        } else if (seleccionado === 'LocalizadorActual') {
+            window.location.href = '/index.html';
+        } else if (seleccionado === 'equipoTrabajo') {
+            window.location.href = '/Equipo_trabajo.html';
+        }
+    });
+});
