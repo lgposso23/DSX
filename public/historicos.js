ec8ad7f9720903813ecb8429e9524518ad2d8c03
lgposso23 <163008630+lgposso23@users.noreply.github.com>
2024-04-25 10:52:22 -0500

    Update historicos.js

--- public/historicos.js
+++ public/historicos.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     var mymap = L.map('mapid');
-    var menuDesplegable = document.getElementById('opcionesMenu');
     var polyline = L.polyline([], { color: 'white' }).addTo(mymap);
     var filtrarButton = document.getElementById('filtrarDatos');
     var datosDePolilinea = [];
@@ -13,7 +12,9 @@ document.addEventListener('DOMContentLoaded', () => {
         enableTime: true,
         dateFormat: "Y-m-d H:i",
         defaultDate: "today", // Establece la fecha predeterminada como hoy
-        maxDate: "today", // Limita la fecha máxima a hoy
+        maxDate: new Date(), // Limita la fecha máxima a hoy
+        minuteIncrement: 15, // Salto de 15 minutos
+        allowInput: false, // Evita la entrada manual
         disableMobile: true, // Evita que aparezca el teclado en dispositivos móviles
         onClose: function(selectedDates, dateStr, instance) {
             // Habilita el campo de fecha y hora final y establece su fecha mínima
@@ -27,8 +28,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const flatpickrFinal = flatpickr("#fechahoraFin", {
         enableTime: true,
         dateFormat: "Y-m-d H:i",
-        defaultDate: "today", // Establece la fecha predeterminada como hoy
-        maxDate: "today", // Limita la fecha máxima a hoy
+        defaultDate: new Date(), // Establece la fecha predeterminada como hoy
+        maxDate: new Date(), // Limita la fecha máxima a hoy
+        minuteIncrement: 15, // Salto de 15 minutos
+        allowInput: false, // Evita la entrada manual
         disableMobile: true, // Evita que aparezca el teclado en dispositivos móviles
         onClose: function(selectedDates, dateStr, instance) {
             validarFiltrarButton(); // Valida el botón de filtro después de cada cambio
@@ -149,21 +152,4 @@ document.addEventListener('DOMContentLoaded', () => {
         updateSliderBackground()
         actualizarMarcadorYPopup(this.value);
     });
-
-    menuDesplegable.addEventListener('change', function(event) {
-        // Obtener el valor seleccionado del menú desplegable
-        const seleccionado = event.target.value;
-
-        // Verificar el valor seleccionado y redirigir a la página correspondiente
-        if (seleccionado === 'rastreoHistoricos') {
-            window.location.href = '/historicos.html';
-        } else if (seleccionado === 'Principal') {
-            window.location.href = '/principal.html';
-        } else if (seleccionado === 'LocalizadorActual') {
-            window.location.href = '/index.html';
-        } else if (seleccionado === 'equipoTrabajo') {
-            window.location.href = '/Equipo_trabajo.html';
-        }
-    });
-    
 });
