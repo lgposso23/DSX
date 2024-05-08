0b3fb1c680b278e3cdd774d5e5e89a10d1e3055e
lgposso23 <163008630+lgposso23@users.noreply.github.com>
2024-05-08 11:26:20 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     var myIcon2 = L.icon({
         iconUrl: 'pics/Carro2.png',
-        iconSize: [10, 10], // tamaño del ícono
+        iconSize: [20, 20], // tamaño del ícono
         iconAnchor: [0, 0], // punto del ícono que corresponderá a la ubicación del marcador
         popupAnchor: [-3, -76] // punto desde el que se abrirá el popup en relación al ícono
     });
